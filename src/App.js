7cd96ca0110b6862768bf438bf0f871587496a23
angu95600 <112316762+angu95600@users.noreply.github.com>
2023-11-18 12:42:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/Pathfindingvisualizer'
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
